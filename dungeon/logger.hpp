7b0db44299d370d069f9c03099d37c29721b11d0
Haoran Wang <vitowang1998@gmail.com>
2020-01-04 00:00:42 -0500

    Create logger.hpp

---
+++ dungeon/logger.hpp
@@ -0,0 +1,14 @@
+//
+//  logger.hpp
+//  dungeon
+//
+//  Created by Haoran Wang on 2020-01-03.
+//  Copyright © 2020 Haoran Wang. All rights reserved.
+//
+
+#ifndef logger_hpp
+#define logger_hpp
+
+#include <stdio.h>
+
+#endif /* logger_hpp */
