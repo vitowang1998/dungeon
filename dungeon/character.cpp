9ddce5c8d1a0baf58795e1f4eb68cec4013a7d65
Haoran Wang <vitowang1998@gmail.com>
2020-01-04 00:00:47 -0500

    Create character.cpp

---
+++ dungeon/character.cpp
@@ -0,0 +1,21 @@
+//
+//  character.cpp
+//  dungeon
+//
+//  Created by Haoran Wang on 2020-01-03.
+//  Copyright © 2020 Haoran Wang. All rights reserved.
+//
+
+#include "character.hpp"
+
+void Character::setHP(int value) {
+    hp = value;
+}
+
+void Character::setATK(int value) {
+    atk = value;
+}
+
+void Character::setDEF(int value) {
+    def = value;
+}
