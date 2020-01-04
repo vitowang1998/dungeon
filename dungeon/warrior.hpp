10893c42a278a0912c0c43662ffb197ee39ca49d
Haoran Wang <vitowang1998@gmail.com>
2020-01-04 00:00:32 -0500

    Create warrior.hpp

---
+++ dungeon/warrior.hpp
@@ -0,0 +1,22 @@
+//
+//  warrior.hpp
+//  dungeon
+//
+//  Created by Haoran Wang on 2020-01-03.
+//  Copyright © 2020 Haoran Wang. All rights reserved.
+//
+
+#ifndef warrior_hpp
+#define warrior_hpp
+
+#include <stdio.h>
+#include <iostream>
+#include "player.hpp"
+
+class Warrior: public Player {
+public:
+    // constructor
+    Warrior();
+};
+
+#endif /* warrior_hpp */
