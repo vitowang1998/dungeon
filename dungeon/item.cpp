cf0cc9950153a08ded8eeec8570ee8543887c14b
Haoran Wang <vitowang1998@gmail.com>
2020-01-04 00:29:09 -0500

    Add abstract class item.

---
+++ dungeon/item.cpp
@@ -0,0 +1,9 @@
+//
+//  item.cpp
+//  dungeon
+//
+//  Created by Haoran Wang on 2020-01-04.
+//  Copyright © 2020 Haoran Wang. All rights reserved.
+//
+
+#include "item.hpp"
---
+++ dungeon/item.hpp
@@ -0,0 +1,19 @@
+//
+//  item.hpp
+//  dungeon
+//
+//  Created by Haoran Wang on 2020-01-04.
+//  Copyright © 2020 Haoran Wang. All rights reserved.
+//
+
+#ifndef item_hpp
+#define item_hpp
+
+#include <stdio.h>
+
+class Item {
+public:
+    virtual void print() = 0;
+};
+
+#endif /* item_hpp */
